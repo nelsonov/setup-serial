3bedabbc5ca57aab78cf884872512e8e529659e7
Lee Nelson <lnelson@nelnet.org>
2021-12-08 22:50:52 -0800

    Create unique id in each terminal and buffer

--- setup-serial.el
+++ setup-serial.el
@@ -3,6 +3,7 @@
 ;;
 (defvar serialspeed "115200")
 (defvar serialport "/dev/ttyACM0")
+(defvar serialbasename "serial")
 (require 'term)
 
 
@@ -19,21 +20,28 @@
 	     serialspeed)
      nil nil
      serialspeed)))
-  (set 'bufferid (get-buffer-create "serialbuffer"))
+  (setq uniqueid (format "%04x" (random (expt 16 4))))
+  (setq serialid (concat serialbasename "-" uniqueid))
+  (setq buffname (concat serialid "-buffer"))
+  (setq termname (concat serialid "-term"))
+  (set 'bufferid (get-buffer-create buffname))
   (make-serial-process
    :speed (string-to-number serialspeed)
    :port serialport
-   :name "serialterm"
-   :buffer "serialbuffer")
-  (switch-to-buffer "serialbuffer")
-  (with-current-buffer "serialbuffer"
+   :name termname
+   :buffer buffname)
+  (switch-to-buffer buffname)
+  (with-current-buffer buffname
     (term-mode))
-  (with-current-buffer "serialbuffer"
+  (with-current-buffer buffname
     (term-char-mode))
   (local-set-key (kbd "M-r") #'resetserial)
   (local-set-key (kbd "M-k") #'killserial)
   (local-set-key (kbd "M-x") #'execute-extended-command)
   (local-set-key (kbd "M-o") #'ace-window)
+  (make-variable-buffer-local 'serialid)
+  (make-variable-buffer-local 'buffname)
+  (make-variable-buffer-local 'termname)
   (message "Started Serial Terminal"))
 
 (defun resetserial ()
